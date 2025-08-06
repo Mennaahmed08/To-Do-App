7fd5c3a1e3c3e7da8f7d41fd36a2294030ff6019
Mennaahmed08 <menna128492@gmail.com>
2025-08-06 21:36:48 +0300

    Update landing page fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './hooks/AuthContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/To-Do-App/">
       <AuthProvider>
         <App/>
       </AuthProvider>
